c97c6fcd248d254cd35fbb302687962839e22a6c
peterxu30 <peterxu30@gmail.com>
2015-08-10 18:09:11 -0700

    Spacing

--- core/src/com/hotboxx/GameMain.java
+++ core/src/com/hotboxx/GameMain.java
@@ -12,8 +12,10 @@ import com.runninggame.utilities.Config;
  * 
  */
 public class GameMain extends Game {
+	
 	/** Game title */
 	public static final String TITLE = "HotBox X";
+	 
 	/** Game version number */
 	public static final String VERSION = "8.8.15";
 	
